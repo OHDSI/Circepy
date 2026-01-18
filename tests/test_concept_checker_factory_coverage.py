e8f805e72b03cc195b3d611ee2f181f0fe928d2e
Jamie Gilbert <jamie.gilbert@azimov.co.uk>
2026-01-18 15:02:12 -0800

    Test coverage improvements

---
+++ tests/test_concept_checker_factory_coverage.py
@@ -0,0 +1,283 @@
+
+import unittest
+from unittest.mock import Mock, call
+from circe.check.checkers.concept_checker_factory import ConceptCheckerFactory
+from circe.check.constants import Constants
+from circe.cohortdefinition.criteria import (
+    ConditionEra, ConditionOccurrence, Death, DeviceExposure, DoseEra,
+    DrugEra, DrugExposure, Measurement, Observation, ObservationPeriod,
+    ProcedureOccurrence, Specimen, VisitOccurrence, PayerPlanPeriod,
+    DemographicCriteria
+)
+
+class TestConceptCheckerFactoryCoverage(unittest.TestCase):
+    def setUp(self):
+        self.reporter = Mock()
+        self.group_name = "Test Group"
+        self.factory = ConceptCheckerFactory.get_factory(self.reporter, self.group_name)
+
+    def test_check_condition_era(self):
+        # Gender is checked
+        c = ConditionEra(codeset_id=0, gender=[])
+        self.factory.check(c)
+        self.reporter.assert_called_with(
+            self.factory.WARNING_EMPTY_VALUE,
+            self.group_name,
+            Constants.Criteria.CONDITION_ERA,
+            Constants.Attributes.GENDER_ATTR
+        )
+
+    def test_check_condition_occurrence(self):
+        c = ConditionOccurrence(
+            codeset_id=0,
+            condition_type=[],
+            gender=[],
+            provider_specialty=[],
+            visit_type=[]
+        )
+        self.factory.check(c)
+        # Should report all 4
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.CONDITION_OCCURRENCE, Constants.Attributes.CONDITION_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.CONDITION_OCCURRENCE, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.CONDITION_OCCURRENCE, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.CONDITION_OCCURRENCE, Constants.Attributes.VISIT_TYPE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_death(self):
+        c = Death(
+            codeset_id=0,
+            death_type=[],
+            gender=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEATH, Constants.Attributes.DEATH_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEATH, Constants.Attributes.GENDER_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_device_exposure(self):
+        c = DeviceExposure(
+            codeset_id=0,
+            device_type=[],
+            gender=[],
+            provider_specialty=[],
+            visit_type=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEVICE_EXPOSURE, Constants.Attributes.DEVICE_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEVICE_EXPOSURE, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEVICE_EXPOSURE, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEVICE_EXPOSURE, Constants.Attributes.VISIT_TYPE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_dose_era(self):
+        c = DoseEra(
+            codeset_id=0,
+            unit=[],
+            gender=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DOSE_ERA, Constants.Attributes.UNIT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DOSE_ERA, Constants.Attributes.GENDER_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_drug_era(self):
+        c = DrugEra(
+            codeset_id=0,
+            gender=[]
+        )
+        self.factory.check(c)
+        self.reporter.assert_called_with(
+            self.factory.WARNING_EMPTY_VALUE, 
+            self.group_name, 
+            Constants.Criteria.DRUG_ERA, 
+            Constants.Attributes.GENDER_ATTR
+        )
+
+    def test_check_drug_exposure(self):
+        c = DrugExposure(
+            codeset_id=0,
+            drug_type=[],
+            route_concept=[],
+            dose_unit=[],
+            gender=[],
+            provider_specialty=[],
+            visit_type=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DRUG_EXPOSURE, Constants.Attributes.DRUG_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DRUG_EXPOSURE, Constants.Attributes.ROUTE_CONCEPT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DRUG_EXPOSURE, Constants.Attributes.DOSE_UNIT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DRUG_EXPOSURE, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DRUG_EXPOSURE, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DRUG_EXPOSURE, Constants.Attributes.VISIT_TYPE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_measurement(self):
+        c = Measurement(
+            codeset_id=0,
+            measurement_type=[],
+            operator=[],
+            value_as_concept=[],
+            unit=[],
+            gender=[],
+            provider_specialty=[],
+            visit_type=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.MEASUREMENT_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.OPERATOR_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.VALUE_AS_CONCEPT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.UNIT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.MEASUREMENT, Constants.Attributes.VISIT_TYPE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_observation(self):
+        c = Observation(
+            codeset_id=0,
+            observation_type=[],
+            value_as_concept=[],
+            qualifier=[],
+            unit=[],
+            gender=[],
+            provider_specialty=[],
+            visit_type=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.OBSERVATION_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.VALUE_AS_CONCEPT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.QUALIFIER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.UNIT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.OBSERVATION, Constants.Attributes.VISIT_TYPE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_observation_period(self):
+        c = ObservationPeriod(
+            period_type=[]
+        )
+        self.factory.check(c)
+        self.reporter.assert_called_with(
+            self.factory.WARNING_EMPTY_VALUE,
+            self.group_name,
+            Constants.Criteria.OBSERVATION_PERIOD,
+            Constants.Attributes.PERIOD_TYPE_ATTR
+        )
+
+    def test_check_procedure_occurrence(self):
+        c = ProcedureOccurrence(
+            codeset_id=0,
+            procedure_type=[],
+            modifier=[],
+            gender=[],
+            provider_specialty=[],
+            visit_type=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.PROCEDURE_OCCURRENCE, Constants.Attributes.PROCEDURE_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.PROCEDURE_OCCURRENCE, Constants.Attributes.MODIFIER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.PROCEDURE_OCCURRENCE, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.PROCEDURE_OCCURRENCE, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.PROCEDURE_OCCURRENCE, Constants.Attributes.VISIT_TYPE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_specimen(self):
+        c = Specimen(
+            codeset_id=0,
+            specimen_type=[],
+            unit=[],
+            anatomic_site=[],
+            disease_status=[],
+            gender=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.SPECIMEN, Constants.Attributes.SPECIMEN_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.SPECIMEN, Constants.Attributes.UNIT_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.SPECIMEN, Constants.Attributes.ANATOMIC_SITE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.SPECIMEN, Constants.Attributes.DISEASE_STATUS_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.SPECIMEN, Constants.Attributes.GENDER_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_visit_occurrence(self):
+        c = VisitOccurrence(
+            codeset_id=0,
+            visit_type=[],
+            gender=[],
+            provider_specialty=[],
+            place_of_service=[]
+        )
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.VISIT_OCCURRENCE, Constants.Attributes.VISIT_TYPE_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.VISIT_OCCURRENCE, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.VISIT_OCCURRENCE, Constants.Attributes.PROVIDER_SPECIALITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.VISIT_OCCURRENCE, Constants.Attributes.PLACE_OF_SERVICE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_check_payer_plan_period(self):
+        c = PayerPlanPeriod(
+            gender=[]
+        )
+        self.factory.check(c)
+        self.reporter.assert_called_with(
+            self.factory.WARNING_EMPTY_VALUE,
+            self.group_name,
+            Constants.Criteria.PAYER_PLAN_PERIOD,
+            Constants.Attributes.GENDER_ATTR
+        )
+
+    def test_check_demographic_criteria(self):
+        c = DemographicCriteria(
+            ethnicity=[],
+            gender=[],
+            race=[]
+        )
+        # DemographicCriteria needs special handling because it's distinct from Criteria in dispatch
+        self.factory.check(c)
+        calls = [
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEMOGRAPHIC, Constants.Attributes.ETHNICITY_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEMOGRAPHIC, Constants.Attributes.GENDER_ATTR),
+            call(self.factory.WARNING_EMPTY_VALUE, self.group_name, Constants.Criteria.DEMOGRAPHIC, Constants.Attributes.RACE_ATTR),
+        ]
+        self.reporter.assert_has_calls(calls, any_order=True)
+
+    def test_default_check(self):
+        # Use LocationRegion which is not handled by ConceptCheckerFactory
+        from circe.cohortdefinition.criteria import LocationRegion
+        
+        c = LocationRegion()
+        self.factory.check(c)
+        # Should not call reporter
+        self.reporter.assert_not_called()
+
+    def test_check_valid_concepts(self):
+        # Test that populated lists do not trigger warnings
+        from circe.vocabulary.concept import Concept
+        c = ConditionEra(
+            codeset_id=0,
+            gender=[Concept(concept_id=1, concept_name="Male", domain_id="Gender", vocabulary_id="Gender")]
+        )
+        self.factory.check(c)
+        self.reporter.assert_not_called()
+
---
+++ tests/test_markdown_render_coverage.py
@@ -0,0 +1,110 @@
+import unittest
+import json
+from circe.cohortdefinition.printfriendly.markdown_render import MarkdownRender
+from circe.cohortdefinition.cohort import CohortExpression
+from circe.vocabulary.concept import ConceptSet
+
+class TestMarkdownRenderCoverage(unittest.TestCase):
+    """
+    Tests specifically targeting edge cases and error handling in MarkdownRender
+    to improve code coverage.
+    """
+
+    def setUp(self):
+        self.renderer = MarkdownRender()
+
+    def test_render_cohort_expression_string_input(self):
+        # Line 83: string input
+        cohort_json = '{"title": "Test String Input", "primaryCriteria": {"observationWindow": {"priorDays": 0, "postDays": 0}, "primaryEvents": []}}'
+        output = self.renderer.render_cohort_expression(cohort_json)
+        # Title is not rendered in the template, so check for structure
+        self.assertIn("Cohort Entry Events", output)
+        self.assertIn("Cohort Exit", output)
+
+    def test_render_cohort_expression_with_concept_sets(self):
+        # Line 90: cohort expression has concept sets
+        cohort_json = '{"title": "With CS", "conceptSets": [{"id": 1, "name": "CS1", "expression": {"items": []}}], "primaryCriteria": {"observationWindow": {"priorDays": 0, "postDays": 0}, "primaryEvents": []}}'
+        output = self.renderer.render_cohort_expression(cohort_json, include_concept_sets=True)
+        self.assertIn("Concept Sets", output)
+        self.assertIn("CS1", output)
+
+    def test_render_cohort_expression_empty_input(self):
+        # Line 86: empty input
+        output = self.renderer.render_cohort_expression(None)
+        self.assertIn("Invalid Cohort Expression", output)
+        self.assertIn("No cohort expression provided", output)
+
+    def test_render_concept_set_list_string_input_list(self):
+        # Lines 117-120: string input as list
+        cs_list_json = '[{"id": 1, "name": "CS1", "expression": {"items": []}}]'
+        output = self.renderer.render_concept_set_list(cs_list_json)
+        self.assertIn("CS1", output)
+
+    def test_render_concept_set_list_string_input_single(self):
+        # Lines 121-122: string input as single object (edge case for loose JSON)
+        cs_json = '{"id": 2, "name": "CS2", "expression": {"items": []}}'
+        output = self.renderer.render_concept_set_list(cs_json)
+        self.assertIn("CS2", output)
+
+    def test_render_concept_set_list_empty(self):
+        # Line 125: empty concept sets
+        output = self.renderer.render_concept_set_list([])
+        self.assertIn("No concept sets specified", output)
+        
+        output_none = self.renderer.render_concept_set_list(None)
+        self.assertIn("No concept sets specified", output_none)
+
+    def test_render_concept_set_string_input(self):
+        # Lines 147-151: string input for single concept set
+        cs_json = '{"id": 3, "name": "CS3", "expression": {"items": []}}'
+        output = self.renderer.render_concept_set(cs_json)
+        self.assertIn("CS3", output)
+
+    def test_codeset_name_not_found(self):
+        # Line 177: ID not found in concept sets
+        # Setup renderer with some concept sets
+        cs = ConceptSet(id=1, name="Existing CodeSet", expression={"items": []})
+        renderer = MarkdownRender(concept_sets=[cs])
+        
+        # Test ID that doesn't exist
+        name = renderer._codeset_name(999, default_name="Default")
+        self.assertEqual(name, "Default")
+        
+        # Line 175: ID found
+        name_found = renderer._codeset_name(1, default_name="Default")
+        self.assertEqual(name_found, "'Existing CodeSet'")
+        
+        # Line 170: ID is None
+        name_none = renderer._codeset_name(None, default_name="Default")
+        self.assertEqual(name_none, "Default")
+
+    def test_format_date_invalid(self):
+        # Lines 195-197: Invalid date handling
+        # Case 1: Wrong formatting but length 10 string -> triggers ValueError inside strptime -> returns "_invalid date_"
+        self.assertEqual(self.renderer._format_date("2020/01/01"), "_invalid date_") 
+        
+        # Case 2: String that is not length 10 -> returns input as is
+        self.assertEqual(self.renderer._format_date("2020/01"), "2020/01")
+        
+        # Case 3: Non-string -> returns input as is (line 195)
+        self.assertEqual(self.renderer._format_date(12345), 12345)
+
+    def test_format_date_valid(self):
+        # Lines 193-194: Valid date
+        # "2020-01-05" -> "January 5, 2020"
+        self.assertEqual(self.renderer._format_date("2020-01-05"), "January 5, 2020")
+        # Check lstrip("0") behavior
+        self.assertEqual(self.renderer._format_date("2020-12-10"), "December 10, 2020")
+
+    def test_format_number_edge_cases(self):
+        # Line 209: None input
+        self.assertEqual(self.renderer._format_number(None), "")
+        
+        # Line 213: Float that is integer
+        self.assertEqual(self.renderer._format_number(1000.0), "1,000")
+        
+        # Normal float
+        self.assertEqual(self.renderer._format_number(1000.5), "1,000.5")
+
+if __name__ == '__main__':
+    unittest.main()
